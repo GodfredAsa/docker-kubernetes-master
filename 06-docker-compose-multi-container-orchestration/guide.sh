d1360e1a870a9c50d73fbdf25a7c048dfc80cf1a
GodfredAsa <godfred.asumadu.asamoah@gmail.com>
2024-03-03 06:10:56 +0000

    docker-compose elegant container orchestration

---
+++ 06-docker-compose-multi-container-orchestration/guide.sh
@@ -0,0 +1,14 @@
+# Services refers to containers 
+# With docker-compose when a service is stop the container is auto-removed.
+
+# Docker-compose automatically adds all the services to the same network althugh you coulsd specify 
+
+# Running the docker-compose
+
+docker-compose up -d 
+
+# to force images to be build based on code changes 
+docker-compose up --build 
+
+# to just build the images without starting the containers 
+docker-compose build 
