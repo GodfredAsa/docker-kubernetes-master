ad9df46e42562d4134e4a946f34a5dd98605e608
GodfredAsa <godfred.asumadu.asamoah@gmail.com>
2024-03-04 03:40:24 +0000

    implements containerization of express, mongo and react app

---
+++ 06-docker-compose-multi-container-orchestration/project-setup/setup-guide.sh
@@ -0,0 +1,4 @@
+# MONGODB_CONNECTION_STRING FORMAT = "mongodb://${USERNAME}:${PASSWORD}:${PORT}:${DB_NAME}?authSource=admin"
+# connection_string = "mongodb://max:secret@mongodb100:port/database?authSource=admin"
+
+ docker-compose up --build -d 
